d192c52d96775b50b132a70d9a933f7aee72af1a
PeterChrz <pc@stampedepress.org>
2025-02-11 18:23:18 -0500

    Fancy oneliner

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ def count(file_contents):
     #Dictionary of all letters
     #alpha = {'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0, 'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0, ' ':0, '':0, '.':0, ',':0, '!':0, '?':0, '@':0}    
     #Whole book coming in.
-    alpha = {}
+    alpha = {}  
     #print(file_contents)
     for c in file_contents:
-        if c.lower() in alpha:
-            #value = alpha[c.lower()]
-            #value += 1
-            alpha[c.lower()] += 1
-        else:
-            alpha[c.lower()] = 1
+        alpha[c.lower()] = alpha.get(c.lower(), 0) + 1
+   #     if c.lower() in alpha:
+   #         #value = alpha[c.lower()]
+   #         #value += 1
+   #         alpha[c.lower()] += 1
+   #     else:
+   #         alpha[c.lower()] = 1
 
     return alpha
 
